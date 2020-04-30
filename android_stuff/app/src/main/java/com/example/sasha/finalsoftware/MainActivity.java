c373194e11a18765e0905eccda88d3f48f2522ab
Heather Quinzon <quinz001@d.umn.edu>
2020-04-30 18:15:36 -0500

    mainactivity should go to names now

--- android_stuff/app/src/main/java/com/example/sasha/finalsoftware/MainActivity.java
+++ android_stuff/app/src/main/java/com/example/sasha/finalsoftware/MainActivity.java
@@ -2,18 +2,35 @@ package com.example.sasha.finalsoftware;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+//import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
 import com.example.cs4531.finalsoftware.R;
 
 public class MainActivity extends AppCompatActivity {
-@Override
+
+    private Button namesButton;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        namesButton = (Button)findViewById(R.id.namesButton);
+//        namesButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, NamesListedActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
     public void switchToNamesListed(View myView) {
-        Intent myIntent = new Intent(this, NamesListedActivity.class);
+        Intent myIntent = new Intent(MainActivity.this, NamesListedActivity.class);
         startActivity(myIntent);
     }
 }
