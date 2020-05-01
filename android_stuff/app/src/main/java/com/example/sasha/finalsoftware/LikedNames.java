cb50aea66b3aa1122c2174f0f703c6cb9aba9ad6
Heather Quinzon <quinz001@d.umn.edu>
2020-04-30 21:06:56 -0500

    added a bunch of page layout stuff bcs i was bored

---
+++ android_stuff/app/src/main/java/com/example/sasha/finalsoftware/LikedNames.java
@@ -0,0 +1,14 @@
+package com.example.sasha.finalsoftware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.cs4531.finalsoftware.R;
+
+public class LikedNames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liked_names);
+    }
+}
